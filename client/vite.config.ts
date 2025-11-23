38f7c40175167281c5c3f5efcf0c9cc68eeccbfc
Pranay Tiwari <pranaytiwariprpk@gmail.com>
2025-11-23 23:22:35 +0530

    fixes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "",
+        target: "https://localhost:5000",
         changeOrigin: true,
         secure: false,
       },
