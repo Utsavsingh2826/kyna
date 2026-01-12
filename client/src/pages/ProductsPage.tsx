f97f186fc55cb59a9ac015bdf9f2866187395a89
Pranay Tiwari <pranaytiwariprpk@gmail.com>
2026-01-12 17:58:52 +0530

    metal colors

--- client/src/pages/ProductsPage.tsx
+++ client/src/pages/ProductsPage.tsx
@@ -2667,7 +2667,7 @@ export default function ProductsPage({ category }: { category: MainCategory }) {
                                     : metal === "SILVER"
                                       ? "/colors/white.png"
                                       : metal === "PLATINUM"
-                                        ? "/colors/platinum.png"
+                                        ? "/colors/white.png"
                                         : metal === "ROSE GOLD"
                                           ? "/colors/rose-gold.png"
                                           : metal === "WHITE GOLD"
