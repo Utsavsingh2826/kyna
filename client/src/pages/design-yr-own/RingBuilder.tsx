a06af9f350ec1ba49e785821e98a22029703c6f2
Pranay Tiwari <pranaytiwariprpk@gmail.com>
2026-02-23 22:42:31 +0530

    padding for video

--- client/src/pages/design-yr-own/RingBuilder.tsx
+++ client/src/pages/design-yr-own/RingBuilder.tsx
@@ -1044,7 +1044,7 @@ export default function RingBuilder() {
         rightColumn={
           <div className="space-y-6">
             {/* Ring Video/Image Display */}
-            <div className="rounded-lg p-8 flex items-center justify-center min-h-64 relative">
+            <div className="rounded-lg flex items-center justify-center min-h-64 relative">
               {isVideo("/vid.mp4") ? (
                 <div 
                   className="relative w-full h-full flex flex-col items-center justify-center gap-4 cursor-pointer group"
